8ec3d883e64d8b9bcb14ed1321c9090b624f42ab
tdtappe <tappe@transdata.net>
2020-02-28 14:23:55 +0100

    Show ajaxStatus problem after updating form with deferred outputPanel.

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,40 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 
-@ManagedBean(name = "testView")
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
-    }
 
-    public String getTestString() {
-        return testString;
-    }
+	private String testString;
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+	@PostConstruct
+	public void init() {
+		testString = "Welcome to PrimeFaces!!!";
+	}
+
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+
+	public String getSlowData() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "I was expensive calculation!";
+	}
+
+	public void doSomething() {
+	}
+
 }
